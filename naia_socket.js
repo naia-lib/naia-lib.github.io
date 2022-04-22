a174d799377ce0d61df32c3dff3d59d38ac7f86b
Connor Carpenter <connorcarpenter@gmail.com>
2022-04-21 23:14:38 -0500

    naia_socket.js -> 0.10.0

--- naia_socket.js
+++ naia_socket.js
@@ -1,1 +1,232 @@
-const naia_socket={channel:null,encoder:new TextEncoder,decoder:new TextDecoder("utf-8"),dropped_outgoing_messages:[],js_objects:{},unique_js_id:0,plugin:function(e){e.env.naia_connect=function(e,n){naia_socket.connect(e,n)},e.env.naia_send=function(e){naia_socket.send(e)},e.env.naia_resend_dropped_messages=function(){naia_socket.resend_dropped_messages()},e.env.naia_create_string=function(e,n){return naia_socket.js_create_string(e,n)},e.env.naia_unwrap_to_str=function(e,n,t){naia_socket.js_unwrap_to_str(e,n,t)},e.env.naia_string_length=function(e){return naia_socket.js_string_length(e)},e.env.naia_create_u8_array=function(e,n){return naia_socket.js_create_u8_array(e,n)},e.env.naia_unwrap_to_u8_array=function(e,n,t){naia_socket.js_unwrap_to_u8_array(e,n,t)},e.env.naia_u8_array_length=function(e){return naia_socket.js_u8_array_length(e)},e.env.naia_free_object=function(e){naia_socket.js_free_object(e)},e.env.naia_random=function(){return Math.random()},e.env.naia_now=function(){return Date.now()}},connect:function(e,n){let t=naia_socket.get_js_object(e)+naia_socket.get_js_object(n),r=new RTCPeerConnection({iceServers:[{urls:["stun:stun.l.google.com:19302"]}]});this.channel=r.createDataChannel("data",{ordered:!1,maxRetransmits:0}),this.channel.binaryType="arraybuffer",this.channel.onopen=function(){naia_socket.channel.onmessage=function(e){let n=new Uint8Array(e.data);wasm_exports.receive(naia_socket.js_object(n))}},this.channel.onerror=function(e){naia_socket.error("data channel error",e.message)},r.onicecandidate=function(e){e.candidate?console.log("received ice candidate",e.candidate):console.log("all local candidates received")},r.createOffer().then(function(e){return r.setLocalDescription(e)}).then(function(){let e=new XMLHttpRequest;e.open("POST",t),e.onload=function(){if(200===e.status){let n=JSON.parse(e.responseText);r.setRemoteDescription(new RTCSessionDescription(n.answer)).then(function(){let e=n.candidate;wasm_exports.receive_candidate(naia_socket.js_object(JSON.stringify(e.candidate)));let t=new RTCIceCandidate(e);r.addIceCandidate(t).then(function(){console.log("add ice candidate success")}).catch(function(e){naia_socket.error("error during 'addIceCandidate'",e)})}).catch(function(e){naia_socket.error("error during 'setRemoteDescription'",e)})}else{let n="error sending POST request to "+t;naia_socket.error(n,{response_status:e.status})}},e.onerror=function(e){let n="error sending POST request to "+t;naia_socket.error(n,e)},e.send(r.localDescription.sdp)}).catch(function(e){naia_socket.error("error during 'createOffer'",e)})},error:function(e,n){n.naia_desc=e,wasm_exports.error(this.js_object(JSON.stringify(n)))},send:function(e){let n=naia_socket.get_js_object(e);this.send_u8_array(n)},resend_dropped_messages:function(){if(this.channel&&this.dropped_outgoing_messages.length>0){let e=this.dropped_outgoing_messages;this.dropped_outgoing_messages=[];for(let n=0;n<e.length;n+=1)this.send_u8_array(Uint8Array.from(e[n]))}},send_str:function(e){if(this.channel)try{this.channel.send(this.encoder.encode(e))}catch(n){this.error("send_str() error. Adding to re-send queue.",n.message),this.dropped_outgoing_messages.push(e)}else this.dropped_outgoing_messages.push(e)},js_create_string:function(e,n){let t=UTF8ToString(e,n);return this.js_object(t)},js_unwrap_to_str:function(e,n,t){let r=this.js_objects[e],s=this.toUTF8Array(r),a=s.length,o=new Uint8Array(wasm_memory.buffer,n,t);for(let e=0;e<a;e++)o[e]=s[e]},js_string_length:function(e){let n=this.js_objects[e];return this.toUTF8Array(n).length},send_u8_array:function(e){if(this.channel)try{this.channel.send(e)}catch(n){this.error("send_u8_array() error. Adding to re-send queue.",n.message),this.dropped_outgoing_messages.push(Array.from(e))}else this.dropped_outgoing_messages.push(Array.from(e))},js_create_u8_array:function(e,n){let t=new Uint8Array(wasm_memory.buffer,e,n);return this.js_object(t)},js_unwrap_to_u8_array:function(e,n,t){let r=this.js_objects[e],s=r.length,a=new Uint8Array(wasm_memory.buffer,n,t);for(let e=0;e<s;e++)a[e]=r[e]},js_u8_array_length:function(e){return this.js_objects[e].length},js_free_object:function(e){delete this.js_objects[e]},toUTF8Array:function(e){let n=[];for(let t=0;t<e.length;t++){let r=e.charCodeAt(t);r<128?n.push(r):r<2048?n.push(192|r>>6,128|63&r):r<55296||r>=57344?n.push(224|r>>12,128|r>>6&63,128|63&r):(t++,r=65536+((1023&r)<<10|1023&e.charCodeAt(t)),n.push(240|r>>18,128|r>>12&63,128|r>>6&63,128|63&r))}return n},js_object:function(e){let n=this.unique_js_id;return this.js_objects[n]=e,this.unique_js_id+=1,n},get_js_object:function(e){return this.js_objects[e]}};miniquad_add_plugin({register_plugin:naia_socket.plugin});
+const naia_socket = {
+    channel: null,
+    encoder: new TextEncoder(),
+    decoder: new TextDecoder("utf-8"),
+    dropped_outgoing_messages: [],
+    js_objects: {},
+    unique_js_id: 0,
+
+    plugin: function (importObject) {
+        importObject.env.naia_connect = function (address, rtc_path) { naia_socket.connect(address, rtc_path); };
+        importObject.env.naia_send = function (message) { naia_socket.send(message); };
+        importObject.env.naia_resend_dropped_messages = function() { naia_socket.resend_dropped_messages(); };
+        importObject.env.naia_create_string = function (buf, max_len) { return naia_socket.js_create_string(buf, max_len); };
+        importObject.env.naia_unwrap_to_str = function (js_object, buf, max_len) { naia_socket.js_unwrap_to_str(js_object, buf, max_len); };
+        importObject.env.naia_string_length = function (js_object) { return naia_socket.js_string_length(js_object); };
+        importObject.env.naia_create_u8_array = function (buf, max_len) { return naia_socket.js_create_u8_array(buf, max_len); };
+        importObject.env.naia_unwrap_to_u8_array = function (js_object, buf, max_len) { naia_socket.js_unwrap_to_u8_array(js_object, buf, max_len); };
+        importObject.env.naia_u8_array_length = function (js_object) { return naia_socket.js_u8_array_length(js_object); };
+        importObject.env.naia_free_object = function (js_object) { naia_socket.js_free_object(js_object); };
+        importObject.env.naia_random = function () { return Math.random(); };
+        importObject.env.naia_now = function () { return Date.now(); };
+    },
+
+    connect: function (server_socket_address, rtc_path) {
+        let server_socket_address_string = naia_socket.get_js_object(server_socket_address);
+        let rtc_path_string = naia_socket.get_js_object(rtc_path);
+        let SESSION_ADDRESS = server_socket_address_string + rtc_path_string;
+
+        let peer = new RTCPeerConnection({
+            iceServers: [{
+                urls: ["stun:stun.l.google.com:19302"]
+            }]
+        });
+
+        this.channel = peer.createDataChannel("data", {
+            ordered: false,
+            maxRetransmits: 0
+        });
+
+        this.channel.binaryType = "arraybuffer";
+
+        this.channel.onopen = function() {
+            naia_socket.channel.onmessage = function(evt) {
+                let array = new Uint8Array(evt.data);
+                wasm_exports.receive(naia_socket.js_object(array));
+            };
+        };
+
+        this.channel.onerror = function(evt) {
+            naia_socket.error("data channel error", evt.message);
+        };
+
+        peer.onicecandidate = function(evt) {
+            if (evt.candidate) {
+                console.log("received ice candidate", evt.candidate);
+            } else {
+                console.log("all local candidates received");
+            }
+        };
+
+        peer.createOffer().then(function(offer) {
+            return peer.setLocalDescription(offer);
+        }).then(function() {
+            let request = new XMLHttpRequest();
+            request.open("POST", SESSION_ADDRESS);
+            request.onload = function() {
+                if (request.status === 200) {
+                    let response = JSON.parse(request.responseText);
+                    peer.setRemoteDescription(new RTCSessionDescription(response.answer)).then(function() {
+                        let response_candidate = response.candidate;
+                        wasm_exports.receive_candidate(naia_socket.js_object(JSON.stringify(response_candidate.candidate)));
+                        let candidate = new RTCIceCandidate(response_candidate);
+                        peer.addIceCandidate(candidate).then(function() {
+                            console.log("add ice candidate success");
+                        }).catch(function(err) {
+                            naia_socket.error("error during 'addIceCandidate'", err);
+                        });
+                    }).catch(function(err) {
+                        naia_socket.error("error during 'setRemoteDescription'", err);
+                    });
+                } else {
+                    let error_str = "error sending POST request to " + SESSION_ADDRESS;
+                    naia_socket.error(error_str, { response_status: request.status });
+                }
+            };
+            request.onerror = function(err) {
+                let error_str = "error sending POST request to " + SESSION_ADDRESS;
+                naia_socket.error(error_str, err);
+            };
+            request.send(peer.localDescription.sdp);
+        }).catch(function(err) {
+            naia_socket.error("error during 'createOffer'", err);
+        });
+    },
+
+    error: function (desc, err) {
+        err['naia_desc'] = desc;
+        wasm_exports.error(this.js_object(JSON.stringify(err)));
+    },
+
+    send: function (message) {
+        let message_string = naia_socket.get_js_object(message);
+        this.send_u8_array(message_string);
+    },
+
+    resend_dropped_messages: function () {
+        if (this.channel && this.dropped_outgoing_messages.length > 0) {
+            let temp_array = this.dropped_outgoing_messages;
+            this.dropped_outgoing_messages = [];
+            for (let i = 0; i < temp_array.length; i+=1) {
+                this.send_u8_array(Uint8Array.from(temp_array[i]));
+            }
+        }
+    },
+
+    send_str: function (str) {
+        if (this.channel) {
+            try {
+                this.channel.send(this.encoder.encode(str));
+            }
+            catch(err) {
+                this.error("send_str() error. Adding to re-send queue.", err.message);
+                this.dropped_outgoing_messages.push(str);
+            }
+        }
+        else {
+            this.dropped_outgoing_messages.push(str);
+        }
+    },
+
+    js_create_string: function (buf, max_len) {
+        let string = UTF8ToString(buf, max_len);
+        return this.js_object(string);
+    },
+
+    js_unwrap_to_str: function (js_object, buf, max_len) {
+        let str = this.js_objects[js_object];
+        let utf8array = this.toUTF8Array(str);
+        let length = utf8array.length;
+        let dest = new Uint8Array(wasm_memory.buffer, buf, max_len);
+        for (let i = 0; i < length; i++) {
+            dest[i] = utf8array[i];
+        }
+    },
+
+    js_string_length: function (js_object) {
+        let str = this.js_objects[js_object];
+        return this.toUTF8Array(str).length;
+    },
+
+    send_u8_array: function (str) {
+        if (this.channel) {
+            try {
+                this.channel.send(str);
+            }
+            catch(err) {
+                this.error("send_u8_array() error. Adding to re-send queue.", err.message);
+                this.dropped_outgoing_messages.push(Array.from(str));
+            }
+        }
+        else {
+            this.dropped_outgoing_messages.push(Array.from(str));
+        }
+    },
+
+    js_create_u8_array: function (buf, max_len) {
+        let u8Array = new Uint8Array(wasm_memory.buffer, buf, max_len);
+        return this.js_object(u8Array);
+    },
+
+    js_unwrap_to_u8_array: function (js_object, buf, max_len) {
+        let str = this.js_objects[js_object];
+        let length = str.length;
+        let dest = new Uint8Array(wasm_memory.buffer, buf, max_len);
+        for (let i = 0; i < length; i++) {
+            dest[i] = str[i];
+        }
+    },
+
+    js_u8_array_length: function (js_object) {
+        let str = this.js_objects[js_object];
+        return str.length;
+    },
+
+    js_free_object: function (js_object) {
+        delete this.js_objects[js_object];
+    },
+
+    toUTF8Array: function (str) {
+        let utf8 = [];
+        for (let i = 0; i < str.length; i++) {
+            let charcode = str.charCodeAt(i);
+            if (charcode < 0x80) utf8.push(charcode);
+            else if (charcode < 0x800) {
+                utf8.push(0xc0 | (charcode >> 6),
+                    0x80 | (charcode & 0x3f));
+            }
+            else if (charcode < 0xd800 || charcode >= 0xe000) {
+                utf8.push(0xe0 | (charcode >> 12),
+                    0x80 | ((charcode >> 6) & 0x3f),
+                    0x80 | (charcode & 0x3f));
+            }
+            // surrogate pair
+            else {
+                i++;
+                // UTF-16 encodes 0x10000-0x10FFFF by
+                // subtracting 0x10000 and splitting the
+                // 20 bits of 0x0-0xFFFFF into two halves
+                charcode = 0x10000 + (((charcode & 0x3ff) << 10)
+                    | (str.charCodeAt(i) & 0x3ff))
+                utf8.push(0xf0 | (charcode >> 18),
+                    0x80 | ((charcode >> 12) & 0x3f),
+                    0x80 | ((charcode >> 6) & 0x3f),
+                    0x80 | (charcode & 0x3f));
+            }
+        }
+        return utf8;
+    },
+
+    js_object: function (obj) {
+        let id = this.unique_js_id;
+        this.js_objects[id] = obj;
+        this.unique_js_id += 1;
+        return id;
+    },
+
+    get_js_object: function (id) {
+        return this.js_objects[id];
+    }
+};
+
+miniquad_add_plugin({ register_plugin: naia_socket.plugin, version: "0.10.0", name: "naia_socket" });
